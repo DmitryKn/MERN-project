0255324015d43c45c944e4799ccc522adc5194f1
DmitryKn <4dmitry.kn@gmail.com>
2019-11-27 11:35:09 -0500

    Navigation + SideDrawer

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MERNapplication
+# MERN-project.
+
+Frontend: React
+Backend: Node.js + Express + Mongo DB
+
+## To start project:
+
+in frontend folder:
+
+> "npm i", "npm start".
+
+in backend folder:
+
+> "npm i", "npm start".
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import Users from './Users/Pages/Users';
-import NewPlace from './Places/Pages/NewPlace';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+
+import MainNavigation from "./Shared/Components/Navigation/MainNavigation";
+import Users from "./Users/Pages/Users";
+import NewPlace from "./Places/Pages/NewPlace";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 const App = () => {
-  return <div>
-    <Switch>
-      <Route exact path='/' component={Users}/>
-      <Route exact path='/places/new' component={NewPlace}/>
-      <Redirect to='/'/>
-    </Switch>
-  </div>;
-}
+  return (
+    <div>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Users} />
+          <Route exact path="/places/new" component={NewPlace} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
