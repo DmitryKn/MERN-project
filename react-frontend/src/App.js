e435115dea8726a5be8fa2c33468d6545295805c
DmitryKn <4dmitry.kn@gmail.com>
2019-12-02 15:16:47 -0500

    Dynamic routing user-places page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import MainNavigation from "./Shared/Components/Navigation/MainNavigation";
 import Users from "./Users/Pages/Users";
+import UserPlaces from "./Places/Pages/UserPlaces";
 import NewPlace from "./Places/Pages/NewPlace";
 import { Route, Redirect, Switch } from "react-router-dom";
 
@@ -12,6 +13,7 @@ const App = () => {
       <main>
         <Switch>
           <Route exact path="/" component={Users} />
+          <Route exact path="/:userId/places" component={UserPlaces} />
           <Route exact path="/places/new" component={NewPlace} />
           <Redirect to="/" />
         </Switch>
--- react-frontend/src/Users/Pages/Users.js
+++ react-frontend/src/Users/Pages/Users.js
@@ -4,14 +4,14 @@ import UserList from "../Components/UsersList";
 const Users = () => {
   const USERS = [
     {
-      id: 1,
-      name: "Dimko Rcvsadf",
+      id: "u1",
+      name: "Dimko",
       image: "https://a.wattpad.com/cover/84608722-352-k886345.jpg",
       places: 3
     },
     {
-      id: 2,
-      name: "Pashko Rcvsadf",
+      id: "u2",
+      name: "Pashko",
       image:
         "https://i.pinimg.com/originals/c0/b7/7f/c0b77faeb2cb3e67fd1b423c4535f6c3.jpg",
       places: 1
