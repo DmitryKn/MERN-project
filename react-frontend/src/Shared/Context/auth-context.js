245d1b24b8c06d5efa78435f6d86580142c9f366
DmitryKn <4dmitry.kn@gmail.com>
2019-12-10 11:36:38 -0500

    Auth Context and NavLinks logic added

--- react-frontend/src/Shared/Context/auth-context.js
+++ react-frontend/src/Shared/Context/auth-context.js
@@ -7,7 +7,10 @@ export const AuthContext = createContext({
 });
 
 /*
-1) create Context
-2) add {AuthContext} to App
-3) wrap with <AuthContext.Provider value={}> all elements + it is good to use  {useState}
-*/
+  1) create Context
+  2) add {AuthContext} to App
+  3) wrap with <AuthContext.Provider value={}> all elements + it is good to use  {useState}
+  4) add {AuthContext} to component where need state + { useContext } for listening state
+    const auth = useContext(AuthContext);
+
+  */
