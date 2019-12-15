cfee08f38dc0fbb00942b72f8c6361f0c252d029
DmitryKn <4dmitry.kn@gmail.com>
2019-12-15 14:27:29 -0500

    separate mapping routes and logic controllers

---
+++ node-backend/controllers/places-controller.js
@@ -0,0 +1,47 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Emp St Build",
+    discription: "OPne",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: "West 34th Street, New York, NY, USA",
+    creator: "u1"
+  }
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const specificPlace = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  if (!specificPlace) {
+    //create an error according by error middleware + error class
+    throw new HttpError("Could not find the place for the provided id", 404);
+  } else {
+    res.json({ place: specificPlace });
+  }
+};
+
+const getPlaceByUserId = (req, res) => {
+  const userId = req.params.uid;
+  const listOfPlacesByUser = DUMMY_PLACES.filter(u => {
+    return u.creator === userId;
+  });
+
+  if (listOfPlacesByUser.length === 0) {
+    throw new HttpError(
+      "Could not find the place for the provided user id",
+      404
+    );
+  } else {
+    res.json({ places: listOfPlacesByUser });
+  }
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
