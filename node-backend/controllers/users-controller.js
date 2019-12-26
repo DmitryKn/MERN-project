ad29508ba3fcd4da2d881b673e459dc205b478e1
DmitryKn <4dmitry.kn@gmail.com>
2019-12-26 15:19:54 -0500

    login to database and error handling

--- node-backend/controllers/users-controller.js
+++ node-backend/controllers/users-controller.js
@@ -64,22 +64,26 @@ const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   let existingUser;
+
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError("Login failed.", 500);
+    const error = new HttpError(
+      "Loggin in failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
   if (!existingUser || existingUser.password !== password) {
     const error = new HttpError(
-      "Invalid credentials, could not log you in",
+      "Invalid credentials, could not log you in.",
       401
     );
     return next(error);
   }
 
-  res.json({ message: "Logged in" });
+  res.json({ message: "Logged in!" });
 };
 
 exports.getUsers = getUsers;
