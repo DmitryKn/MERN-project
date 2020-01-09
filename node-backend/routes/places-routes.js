2e15f126bdc4c8ae1dd0ed42c1acef3e23467931
DmitryKn <4dmitry.kn@gmail.com>
2020-01-09 16:27:17 -0500

    middleware for checking auth and protect routes

--- node-backend/routes/places-routes.js
+++ node-backend/routes/places-routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const { check } = require("express-validator");
 const placesController = require("../controllers/places-controller");
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check_auth");
 
+//open resourse
 router.get("/:pid", placesController.getPlaceById);
-
 router.get("/user/:uid", placesController.getPlacesByUserId);
 
+//protected by auth token
+router.use(checkAuth); //middeware for checking authorization
 router.post(
   "/",
   fileUpload.single("image"),
